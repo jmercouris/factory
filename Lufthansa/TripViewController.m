5c4533f74cb9a04ace4c50c5b544dc11319f880b
John Mercouris <jmercouris@gmail.com>
2015-07-04 13:09:42 +0200

    Adding data model

--- Lufthansa/TripViewController.m
+++ Lufthansa/TripViewController.m
@@ -29,23 +29,6 @@ NSArray *prepTime;
     // Initialize table data
     tableData = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
 
-    // Initialize thumbnails
-    thumbnails = [NSArray arrayWithObjects:@"egg_benedict.jpg", @"mushroom_risotto.jpg", @"full_breakfast.jpg", @"hamburger.jpg", @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg", @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg", @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg", @"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg", @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg", nil];
-    
-    // Initialize Preparation Time
-    prepTime = [NSArray arrayWithObjects:@"30 min", @"30 min", @"20 min", @"30 min", @"10 min", @"1 hour", @"45 min", @"5 min", @"30 min", @"8 min", @"20 min", @"20 min", @"5 min", @"1.5 hour", @"4 hours", @"10 min", nil];
-    
-
-    // Find out the path of recipes.plist
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"recipes" ofType:@"plist"];
-    
-    // Load the file content and read the data into arrays
-    NSDictionary *dict = [[NSDictionary alloc] initWithContentsOfFile:path];
-    tableData = [dict objectForKey:@"RecipeName"];
-    thumbnails = [dict objectForKey:@"Thumbnail"];
-    prepTime = [dict objectForKey:@"PrepTime"];
-
-
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -72,7 +55,8 @@ NSArray *prepTime;
     // indexPath.row ( current index )
     cell.timeLabel.text = @"5:00am - 9:00am";
     cell.priceLabel.text = @"lol";
-    cell.prepTimeLabel.text = [prepTime objectAtIndex:indexPath.row];
+    cell.prepTimeLabel.text = @"haha";
+    //cell.prepTimeLabel.text = [prepTime objectAtIndex:indexPath.row];
     
     return cell;
 }
@@ -82,15 +66,6 @@ NSArray *prepTime;
     NSLog(@"didSelectRowAtIndexPath");
     /*UIAlertView *messageAlert = [[UIAlertView alloc]
      initWithTitle:@"Row Selected" message:@"You've selected a row" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];*/
-    UIAlertView *messageAlert = [[UIAlertView alloc]
-                                 initWithTitle:@"Row Selected" message:[tableData objectAtIndex:indexPath.row] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
-    
-    // Display the Hello World Message
-    [messageAlert show];
-    
-    // Checked the selected row
-    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
-    cell.accessoryType = UITableViewCellAccessoryCheckmark;
     
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
